7627e57f0f67c4fb66a97af0d55f503578fc7f38
ronygolderku <ronygolderku@gmail.com>
2025-08-29 21:32:52 +0800

    sliding

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const form = document.querySelector("form"),
-  statusTxt = form.querySelector(".button-area span");
+// Defensive check for form element
+const form = document.querySelector("form");
+if (form) {
+  const statusTxt = form.querySelector(".button-area span");
 
 
 
-let xhr = new XMLHttpRequest();
-xhr.open("POST", "https://formspree.io/f/mayagkkz", true);
-xhr.onload = () => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    let response = xhr.response;
-    if (response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1) {
-      statusTxt.style.color = "red";
-    } else {
-      form.reset();
-      setTimeout(() => {
-        statusTxt.style.display = "none";
-      }, 3000);
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://formspree.io/f/mayagkkz", true);
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = xhr.response;
+      if (response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1) {
+        statusTxt.style.color = "red";
+      } else {
+        form.reset();
+        setTimeout(() => {
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
     }
-    statusTxt.innerText = response;
-    form.classList.remove("disabled");
   }
+  let formData = new FormData(form);
+  xhr.send(formData);
 }
-let formData = new FormData(form);
-xhr.send(formData);
